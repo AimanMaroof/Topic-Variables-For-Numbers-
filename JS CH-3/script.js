380a3e7fd4976a14c1d6eecb3de7726cf928df19
AimanMaroof <maroofaiman07@gmail.com>
2024-10-02 00:01:06 +0500

    Add files via upload
    Variables For Numbers.

---
+++ JS CH-3/script.js
@@ -0,0 +1,27 @@
+// chapter - 3 (VARIABLES FOR NUMBERS)
+// QS.1 --> Declare a variable height and assign the value 170 to it. Then, add 10 to height and store the result in a new variable newHeight. Display an alert with the new value.
+var height = 170;
+var newHeight = 180;
+alert(newHeight);
+
+
+
+// QS.2 --> Declare two variables, num1 and num2, and assign them values of 50 and 30, respectively. Create a new variable sum that stores the sum of num1 and num2, and display the sum in an alert.
+var num1 = 50;
+var num2 = 30;
+var sum = num1 + num2;
+alert(sum);
+
+
+// QS.3 --> Declare a variable numToBeAdded with the value 20. Then use the following code:Display total in an alert. What is the value of total?
+var numToBeAdded = 20;
+var total = numToBeAdded + 10; // For example, adding 10 to numToBeAdded
+alert(total);
+
+
+// QS.4 --> Declare a variable price with the value 120. Now add tax (10%) to price and display the total price in an alert.
+var price = 120;
+var tax = price * 0.10; // Calculate 10% tax
+var totalPrice = price + tax; // Add tax to the original price
+alert("The total price is: " + totalPrice);
+
